706a5f93e4c90010a6bbe1c369ba24274b851ea5
Milton Edwards <miltone@bcicjamaica.com>
2024-01-01 07:15:53 -0500

    Added new colours

--- app/constants/Colors.ts
+++ app/constants/Colors.ts
@@ -8,6 +8,8 @@ export default {
         tint: tintColorLight,
         tabIconDefault: '#CCCCCC',
         tabIconSelected: tintColorLight,
+        success: "#166534",
+        danger: "#B91C1C",
     },
     dark: {
         text: '#FFFFFF',
@@ -15,5 +17,7 @@ export default {
         tint: tintColorDark,
         tabIconDefault: '#CCCCCC',
         tabIconSelected: tintColorDark,
+        success: "#4ADE80",
+        danger: "#EF1C1C"
     },
 };
