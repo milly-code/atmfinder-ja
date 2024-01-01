249b1bdaa401588569c994909164b56eafebda16
Milton Edwards <miltone@bcicjamaica.com>
2024-01-01 18:14:19 -0500

    added bank icons to map view

--- app/components/MapView/GoogleMapView.tsx
+++ app/components/MapView/GoogleMapView.tsx
@@ -5,7 +5,8 @@ import { MapThemes } from './map-themes';
 import { FontAwesome } from '@expo/vector-icons';
 import { View } from '../themed/View';
 import { atmData } from './map-view-data';
-import { useColorScheme } from 'react-native';
+import { Image, useColorScheme } from 'react-native';
+import Banks from '@app/constants/Banks';
 
 export type MapViewType = {
     scrollEnabled?: boolean,
@@ -21,16 +22,21 @@ export type MapViewType = {
     zoomEnabled?: boolean;
 } & PropsWithChildren
 
-
 export const GoogleMapView = forwardRef<MapView, MapViewType>(({ ...props }, ref) => {
-    const colourScheme = useColorScheme() ?? 'light';
-    const { defaultCamera, children, region, userLocation = {
+    const DEFAULT_REGION = {
+        //Jamaica
         latitude: 17.77027018441539,
         longitude: -77.18976974487305,
-    } } = props;
+        latitudeDelta: 4.550155938686251,
+        longitudeDelta: 2.274669148027897,
+    }
+
+    const colourScheme = useColorScheme() ?? 'light';
+    const { defaultCamera, children, region, userLocation } = props;
     const { followLocation = false, zoomEnabled = true, scrollEnabled = true, } = props;
 
     const mapTheme = MapThemes[colourScheme];
+
 
     return (
         <View className='flex-1 flex'>
@@ -39,24 +45,22 @@ export const GoogleMapView = forwardRef<MapView, MapViewType>(({ ...props }, ref
                 scrollEnabled={scrollEnabled}
                 followsUserLocation={followLocation}
                 zoomControlEnabled={false}
-                showsIndoors={true}
-                showsCompass={true}
+                showsIndoors={false}
+                showsCompass={false}
                 showsPointsOfInterest={false}
                 showsTraffic={false}
                 showsBuildings={false}
-                showsMyLocationButton={true}
-                showsUserLocation={true}
+                showsMyLocationButton={false}
+                showsUserLocation={false}
                 zoomEnabled={zoomEnabled}
                 showsScale={true}
                 camera={defaultCamera}
                 provider={PROVIDER_GOOGLE}
                 style={{ flex: 1 }}
                 customMapStyle={mapTheme}
-                region={region ?? {
-                    latitude: 17.77027018441539,
-                    longitude: -77.18976974487305,
-                    latitudeDelta: 0.160,
-                    longitudeDelta: 0.121,
+                region={region ?? DEFAULT_REGION}
+                onUserLocationChange={(props) => {
+                    // console.log(props.nativeEvent.coordinate, props.nativeEvent.error);
                 }}
                 onMapLoaded={() => {
                 }}
@@ -76,8 +80,8 @@ export const GoogleMapView = forwardRef<MapView, MapViewType>(({ ...props }, ref
                         title="You"
                         tracksViewChanges={false}
                     >
-                        <View className='flex-col items-center justify-center rounded-full bg-gray-100 px-1 py-0.5'>
-                            <FontAwesome name='circle' color={"red"} size={25} />
+                        <View className='bg-red-500 border border-white flex h-6 w-6 rounded-full items-center justify-center'>
+                            <FontAwesome name='location-arrow' color={"white"} size={15} />
                         </View>
 
                     </Marker>
@@ -85,6 +89,7 @@ export const GoogleMapView = forwardRef<MapView, MapViewType>(({ ...props }, ref
 
                 {
                     atmData.map((atm) => {
+                        const bankLogo = Banks.find(item => item.code === atm.atmType)?.image;
                         return (
                             <Marker
                                 tracksViewChanges={false}
@@ -97,7 +102,18 @@ export const GoogleMapView = forwardRef<MapView, MapViewType>(({ ...props }, ref
                                 title={atm.displayName}
                                 description={atm.shortFormattedAddress}
                             >
-                                <FontAwesome name='map-pin' color={"red"} size={25} />
+                                {
+                                    bankLogo ?
+                                        (
+                                            <Image
+                                                source={bankLogo}
+                                                style={{ width: 30, height: 30 }} resizeMode="contain"
+                                            />
+                                        ) :
+                                        (
+                                            <FontAwesome name='map-pin' color={"red"} size={25} />
+                                        )
+                                }
                             </Marker>
                         )
                     })
