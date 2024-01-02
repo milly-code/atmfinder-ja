33782bfaebbe4283b51746f436a5c81356f8a89c
Milton Edwards <miltone@bcicjamaica.com>
2024-01-02 08:40:50 -0500

    Updated bank icons

--- app/constants/Banks.ts
+++ app/constants/Banks.ts
@@ -1,42 +1,49 @@
-export default [
+type Bank = {
+    bank: string; 
+    image: any, 
+    code: ATMType
+}
+
+const banks: Bank[] =  [
     {
         bank: "Scotiabank",
-        image: require("../../assets/banks/bns.png"),
+        image: require("../../assets/banks/bns-2.png"),
         code: "BNS"
     },
     {
         bank: "National Commercial Bank",
-        image: require("../../assets/banks/ncb.png"),
+        image: require("../../assets/banks/ncb-2.png"),
         code: "NCB"
     },
     {
         bank: "Victoria Mutual",
-        image: require("../../assets/banks/vmbs.png"),
+        image: require("../../assets/banks/vmbs-2.png"),
         code: "VM"
     },
     {
         bank: "JMMB",
-        image: require("../../assets/banks/jmmb.png"),
+        image: require("../../assets/banks/jmmb-2.png"),
         code: "JMMB"
     },
     {
         bank: "Jamaica National Bank",
-        image: require("../../assets/banks/jnb.png"),
+        image: require("../../assets/banks/jnb-2.png"),
         code: "JN"
     },
     {
         bank: "First Global Bank",
-        image: require("../../assets/banks/fgb.png"),
+        image: require("../../assets/banks/fgb-2.png"),
         code: "FGB"
     },
     {
         bank: "Sagicor",
-        image: require("../../assets/banks/sagicor.png"),
+        image: require("../../assets/banks/sagicor-2.png"),
         code: "Sagicor"
     },
     {
         bank: "FCIBC",
-        image: require("../../assets/banks/cibc.png"),
+        image: require("../../assets/banks/cibc-2.png"),
         code: "CIBC"
     },
 ]
+export default banks;
