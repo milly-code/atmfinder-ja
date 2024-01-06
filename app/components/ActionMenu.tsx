df25397709fef2e792ae984e29d67f7ac1f2e28c
Milton Edwards <miltone@bcicjamaica.com>
2024-01-05 23:58:03 -0500

    Rearranged Icons in action menu

--- app/components/ActionMenu.tsx
+++ app/components/ActionMenu.tsx
@@ -1,41 +1,39 @@
-import BottomSheet from "@gorhom/bottom-sheet"
-import { forwardRef } from "react"
-import { View } from "./themed/View"
-import { Text } from "./themed/Text"
-import { Pressable, useColorScheme } from "react-native"
-import Colors from "@app/constants/Colors"
-import { FontAwesome } from "@expo/vector-icons"
-import Constants from "expo-constants"
+import { forwardRef } from "react";
+import { View } from "./themed/View";
+import { Text } from "./themed/Text";
+import Constants from "expo-constants";
+import { Pressable } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { BottomSheet } from "./themed/BottomSheet";
+import GorhomBottomSheet from "@gorhom/bottom-sheet";
+import { useColorScheme } from "@app/hooks/useColorScheme";
 
-export const ActionMenu = forwardRef<BottomSheet>((_, bottomSheetRef) => {
-    const colourScheme = useColorScheme() ?? 'light';
+export const ActionMenu = forwardRef<GorhomBottomSheet>((_, bottomSheetRef) => {
+    const { themeColours } = useColorScheme();
     return (
         <BottomSheet
             ref={bottomSheetRef}
             index={-1}
             snapPoints={['30%']}
-            style={{}}
             enablePanDownToClose
-            backgroundStyle={{ backgroundColor: Colors[colourScheme].background }}
-            handleComponent={() => <View className='flex flex-col items-center mt-2'><View className='h-1 rounded-sm w-7 bg-gray-900 dark:bg-gray-500 mb-3'></View></View>}
         >
-            <View className="px-4">
+            <View className="flex-col justify-center w-full px-4 divide-y divide-gray-200/80 dark:divide-gray-500">
                 <Pressable>
-                    <View className="flex-row space-x-3 px-2 pb-4 pt-3 items-center border-b border-gray-500">
+                    <View className="flex-row space-x-3 px-2 py-3.5 items-center">
+                        <FontAwesome name="star" size={20} color={themeColours.tint} />
                         <Text style={{ fontSize: 18 }}>Love the app? Leave a rating</Text>
-                        <FontAwesome name="star" size={20} color={Colors[colourScheme].tint} />
                     </View>
                 </Pressable>
                 <Pressable>
-                    <View className="flex-row space-x-3 px-2 pb-4 pt-3 items-center border-b border-gray-500">
+                    <View className="flex-row space-x-3 px-2 py-3.5 items-center">
+                        <FontAwesome name="comment" size={20} color={themeColours.tint} />
                         <Text style={{ fontSize: 18 }}>Contact Support</Text>
-                        <FontAwesome name="comment" size={20} color={Colors[colourScheme].tint} />
                     </View>
                 </Pressable>
                 <Pressable>
-                    <View className="flex-row space-x-3 px-2 pb-4 pt-3 items-center ">
+                    <View className="flex-row space-x-3 px-2 py-3.5 items-center ">
+                        <FontAwesome name="info-circle" size={20} color={themeColours.tint} />
                         <Text style={{ fontSize: 18 }}>Version: {Constants.expoConfig?.version}</Text>
-                        <FontAwesome name="info-circle" size={20} color={Colors[colourScheme].tint} />
                     </View>
                 </Pressable>
             </View>
