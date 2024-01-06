73558d073f5a1a94312a0c30606bc34561fd6001
Milton Edwards <miltone@bcicjamaica.com>
2024-01-06 00:04:22 -0500

    Updated button styles

--- app/components/themed/Buttons.tsx
+++ app/components/themed/Buttons.tsx
@@ -1,21 +1,34 @@
-import { GestureResponderEvent, Pressable, View } from "react-native"
+import { GestureResponderEvent, Pressable, View, PressableProps, ActivityIndicator } from "react-native"
 import { Text } from "./Text"
 import { FC, PropsWithChildren } from "react"
 import { twMerge } from "tailwind-merge";
 
 type ButtonProps = {
     onPress?: (event: GestureResponderEvent) => void;
-} & PropsWithChildren & View['props']
+    componentClass?: string;
+    isBusy?: boolean;
+} & PropsWithChildren & PressableProps
 
-
-export const ButtonPrimary: FC<ButtonProps> = ({ children, onPress, className, ...props }) => {
+export const ButtonPrimary: FC<ButtonProps> = ({ children, onPress, componentClass, disabled, isBusy, ...props }) => {
 
     return (
-        <Pressable onPress={onPress} className={twMerge(
-            'bg-green-700 rounded-lg px-2 py-2.5 items-center justify-center dark:bg-green-600',
-            className
-        )} {...props}>
-            <Text componentClass='uppercase text-white'>{children}</Text>
+        <Pressable
+            disabled={disabled || isBusy}
+            onPress={onPress}
+            className={twMerge(
+                'bg-green-700 rounded-lg px-2 py-2.5 items-center justify-center dark:bg-green-600',
+                (disabled || isBusy) ? 'opacity-50' : '',
+                componentClass
+            )} {...props}
+        >
+            {
+                isBusy ? (
+                    <ActivityIndicator color={"white"} />
+                ) :
+                    (
+                        <Text componentClass='uppercase text-white'>{children}</Text>
+                    )
+            }
         </Pressable>
     )
 }
